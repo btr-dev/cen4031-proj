24faea17a23e8a5303638e4a5a423cc7d0ca51f4
acgispert <100173638+acgispert@users.noreply.github.com>
2022-02-28 22:19:27 -0500

    added HTML for presentation layer

---
+++ myproject/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article one',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderitin voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatatnon proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'vasanth',
+            'create_date':'04-09-2018',
+        },
+        {
+            'id': 2,
+            'title':'Article two',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderitin voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatatnon proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'vasanth nagarajan',
+            'create_date':'05-09-2018',
+        },
+        {
+            'id': 3,
+            'title':'Article three',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderitin voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatatnon proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'nagarajan vasanth',
+            'create_date':'04-09-2018',
+        }
+]
+    return articles
