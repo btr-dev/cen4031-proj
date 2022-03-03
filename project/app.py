20ccee9bb75aae3ef0cf4f7a746ba1e319c73124
pinntoro <73811486+pinntoro@users.noreply.github.com>
2022-03-02 19:19:28 -0500

    Integrated project layers

--- project/app.py
+++ project/app.py
@@ -1,37 +1,29 @@
-from flask import Flask, request, render_template
-import datetime
-import time
-import os
+from flask import Flask, render_template, request
+from stopWatch import StopWatch
 
 app = Flask(__name__)
+app.debug = True
 
-class StopWatch:
-    def __init__(self, flask_app):
-        self.start_time = 0
-        self.stop_time = 0 
-        self.running = False
-        self.dbmgr = dbManager(flask_app)
+# Create Timer object from stopwatch class 
+timer = StopWatch(app)
+# This would be the return point for the elapsed time data to be displayed to
+# The website table.
+headings = ("ID", "Duration")
+data = timer.retrieveData()
+previous = "0.0"
 
-    def startTimer(self):
-        if self.running == False:
-           self.start_time = time.time() 
-           self.running = True
-        return self.start_time
-    
-    def stopTimer(self):
-        if self.running == True:
-            self.stop_time = time.time()
-            self.dbmgr.add(self.stop_time - self.start_time)
-            self.running = False
-            return self.dbmgr.retrieve()
-        return []
-        
-@app.route("/", methods=["GET","POST"])
-def elaspedTime():
-    if request.method == "POST":
-        elapsedTime = request.form.get("elapsedTime")
-        ###double check if home.html is the correct destination
-    return render_template("home.html",elapsedTime = elapsedTime)
+@app.route('/', methods=["GET", "POST"])
+def index():
+    global headings, data, previous
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'START':
+            timer.startTimer()
+        elif request.form['submit_button'] == 'STOP':
+            data = timer.stopTimer()
+            #This number variable is our elapsed time that is displayed in the
+            # text window.
+            previous = str(data[-1][1])
+    return render_template('home.html', TableHeadings=headings, TableData=data, Number=previous)
 
-if __name__ == '__main__':
-    app.run(debug = True) 
+if __name__ =='__main__':
+    app.run()
